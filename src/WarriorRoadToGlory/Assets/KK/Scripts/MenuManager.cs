ead6489122d23c73a851a619bd5a1cffdbd7a673
krl76 <k.kornilov2005@yandex.ru>
2024-04-02 01:46:50 +0300

    settings 75% (need rework dropdown)

--- src/WarriorRoadToGlory/Assets/KK/Scripts/MenuManager.cs
+++ src/WarriorRoadToGlory/Assets/KK/Scripts/MenuManager.cs
@@ -1,15 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    [Header("Main")]
     [SerializeField] private Transform _head;
     [SerializeField] private GameObject _mainMenu;
+
+    [Header("Settings")] 
+    [SerializeField] private Slider _sliderVolume;
+    [SerializeField] private TextMeshProUGUI _textVolume;
     void Update()
     {
         _mainMenu.transform.LookAt(new Vector3(_head.position.x, _mainMenu.transform.position.y, _head.position.z ));
         _mainMenu.transform.forward *= -1;
+
+        _textVolume.text = Math.Round(_sliderVolume.value).ToString();
     }
 
     void Exit()
