258b7fb2438ebcb54951be6a8172f6750a909d2f
None <141856380+seaG7@users.noreply.github.com>
2024-04-04 19:40:23 +0300

    test meshslicer

---
+++ src/WarriorRoadToGlory/Assets/BzKovSoft/ObjectSlicer/BzSliceConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace BzKovSoft.ObjectSlicer
+{
+	/// <summary>
+	/// This component can be added to object with mesh renderer to configure its behaviour
+	/// </summary>
+	[DisallowMultipleComponent]
+	public class BzSliceConfiguration : MonoBehaviour
+	{
+		public SliceType SliceType;
+		public Material SliceMaterial;
+	}
+
+	public enum SliceType
+	{
+		Slice,
+		Duplicate,
+		KeepOne,
+	}
+}
