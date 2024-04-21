960ee91d98b7065fb11ba81c5fa91b958eff8d9f
seaG7 <neyro.yt@gmail.com>
2024-04-21 16:48:25 +0300

    blood, weapon, hands, 1 rigged enemy

---
+++ src/WarriorRoadToGlory/Assets/MD/ForReports/HeadController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+
+    [SerializeField] private float _power;
+
+    private const string weapon_tag = "Weapon";
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (!collider.CompareTag(weapon_tag))
+            return;
+
+        _rigidbody.isKinematic = false;
+        _rigidbody.AddForce(Vector3.up * _power);
+    }
+}
--- src/WarriorRoadToGlory/Assets/MD/Scripts/HitEffect.cs
+++ src/WarriorRoadToGlory/Assets/MD/Scripts/HitEffect.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Threading;
 using UnityEngine;
 public class HitEffect : MonoBehaviour
     {
-        public GameObject[] hitEffects = new GameObject[5];
-        GameObject hitEffectClone;
+        public GameObject[] hitEffects = new GameObject[1];
+        GameObject _hitEffectClone;
         private const string weaponTag = "Weapon";
-
-        private void OnCollisionEnter(Collision collision)
+        bool _sentinel = false;
+    private void OnCollisionEnter(Collision collision)
         {
             if (collision.transform.tag == weaponTag)
             {
@@ -16,10 +17,18 @@ public class HitEffect : MonoBehaviour
                 foreach (ContactPoint swordHit in collision.contacts)
                 {
                     Vector3 hitPoint = swordHit.point;
-                    int index = Random.Range(0, hitEffects.Length);
-                    hitEffectClone = (GameObject)Instantiate(hitEffects[index], new Vector3(hitPoint.x, hitPoint.y, hitPoint.z), Quaternion.LookRotation(collision.contacts[0].normal));
-                    Destroy(hitEffectClone, 1);
+                    if (!_sentinel)
+                    {
+                        int index = Random.Range(0, hitEffects.Length);
+                        _hitEffectClone = (GameObject)Instantiate(hitEffects[index], new Vector3(hitPoint.x, hitPoint.y, hitPoint.z), Quaternion.LookRotation(-collision.contacts[0].normal));
+                        _sentinel = true;
+                    }
                 }
             }
         }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (_sentinel)
+            _sentinel = false;
     }
+}
