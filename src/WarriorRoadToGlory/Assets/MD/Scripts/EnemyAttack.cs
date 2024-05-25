a50dd7acbe72bcfb589a35f5c57f3edb79bf5b32
seaG7 <neyro.yt@gmail.com>
2024-05-26 01:01:24 +0300

    rome smeshno

--- src/WarriorRoadToGlory/Assets/MD/Scripts/EnemyAttack.cs
+++ src/WarriorRoadToGlory/Assets/MD/Scripts/EnemyAttack.cs
@@ -4,31 +4,22 @@ public class EnemyAttack : StateMachineBehaviour
 {
     Transform player;
     int attackChoice;
-    int lastChoice;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        //EnemyChase.agent.SetDestination(EnemyChase.agent.transform.position);
-        lastChoice = 0;
-        //player = GameObject.FindGameObjectWithTag("Point").transform;
-        attackChoice = Random.Range(1, 4);
-        animator.SetInteger("AttackChoice", attackChoice);
-    }
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        attackChoice = Random.Range(1, 4);
-        while (attackChoice == lastChoice)
+        int cash = Animator.StringToHash("AttackChoice");
+        attackChoice = animator.GetInteger(cash);
+        switch (attackChoice)
         {
-            attackChoice = Random.Range(1, 4);
+            case 1:
+                attackChoice = Random.Range(2, 4);
+                break;
+            case 2:
+                attackChoice = 1;
+                break;
+            case 3:
+                attackChoice = Random.Range(1, 3);
+                break;
         }
         animator.SetInteger("AttackChoice", attackChoice);
-        lastChoice = attackChoice;
-        //animator.transform.LookAt(player);
-        /*float distance = Vector3.Distance(animator.transform.position, player.position);
-        if (distance > EnemyChase.attackRange)
-        {
-            animator.SetBool("isAttacking", false);
-            animator.SetBool("isChasing", true);
-            EnemyChase.agent.SetDestination(player.position);
-        }*/
     }
 }
