89e79c93320861562c4c0a69e228c87fdd97e591
seaG7 <neyro.yt@gmail.com>
2024-05-26 23:58:22 +0300

    boss+minotaur attack

--- src/WarriorRoadToGlory/Assets/MD/Scripts/BossAttack.cs
+++ src/WarriorRoadToGlory/Assets/MD/Scripts/BossAttack.cs
@@ -6,30 +6,31 @@ public class BossAttack : StateMachineBehaviour
 {
     Transform player;
     int attackChoice;
-    int lastChoice;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        lastChoice = 0;
-        //player = GameObject.FindGameObjectWithTag("Point").transform;
-        attackChoice = Random.Range(1, 6);
-        animator.SetInteger("AttackChoice", attackChoice);
-    }
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        attackChoice = Random.Range(1, 6);
-        while (attackChoice == lastChoice)
+        int cash = Animator.StringToHash("AttackChoice");
+        attackChoice = animator.GetInteger(cash);
+        switch (attackChoice)
         {
-            attackChoice = Random.Range(1, 6);
+            case 1:
+                attackChoice = Random.Range(2, 6);
+                break;
+            case 2:
+                attackChoice = Random.Range(3, 6);
+                break;
+            case 3:
+                attackChoice = Random.Range(4, 6);
+                break;
+            case 4:
+                attackChoice = Random.Range(1, 4);
+                break;
+            case 5:
+                attackChoice = Random.Range(1, 5);
+                break;
+            case 6:
+                attackChoice = Random.Range(1, 6);
+                break;
         }
         animator.SetInteger("AttackChoice", attackChoice);
-        lastChoice = attackChoice;
-        //animator.transform.LookAt(player);
-        //float distance = Vector3.Distance(animator.transform.position, player.position);
-        //if (distance > EnemyChase.attackRange)
-        //{
-        //    animator.SetBool("isAttacking", false);
-        //    animator.SetBool("isChasing", true);
-        //    EnemyChase.agent.SetDestination(player.position);
-        //}
     }
 }
--- src/WarriorRoadToGlory/Assets/MD/Scripts/MinotaurAttack.cs
+++ src/WarriorRoadToGlory/Assets/MD/Scripts/MinotaurAttack.cs
@@ -2,31 +2,24 @@ using UnityEngine;
 using UnityEngine.AI;
 public class MinotaurAttack : StateMachineBehaviour
 {
+    Transform player;
     int attackChoice;
-    int lastChoice;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        lastChoice = 0;
-        //player = GameObject.FindGameObjectWithTag("Point").transform;
-        attackChoice = Random.Range(1, 4);
-        animator.SetInteger("AttackChoice", attackChoice);
-    }
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        attackChoice = Random.Range(1, 4);
-        while (attackChoice == lastChoice)
+        int cash = Animator.StringToHash("AttackChoice");
+        attackChoice = animator.GetInteger(cash);
+        switch (attackChoice)
         {
-            attackChoice = Random.Range(1, 4);
+            case 1:
+                attackChoice = Random.Range(2, 4);
+                break;
+            case 2:
+                attackChoice = 1;
+                break;
+            case 3:
+                attackChoice = Random.Range(1, 3);
+                break;
         }
         animator.SetInteger("AttackChoice", attackChoice);
-        lastChoice = attackChoice;
-        //animator.transform.LookAt(player);
-        //float distance = Vector3.Distance(animator.transform.position, player.position);
-        //if (distance > EnemyChase.attackRange)
-        //{
-        //    animator.SetBool("isAttacking", false);
-        //    animator.SetBool("isChasing", true);
-        //    EnemyChase.agent.SetDestination(player.position);
-        //}
     }
 }
